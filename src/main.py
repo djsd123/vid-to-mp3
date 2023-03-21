2088ec4344fb680096df680e698bef1b70cb8f80
Mikael Allison <mikellallison@gmail.com>
2023-03-21 17:55:39 +0000

    Song Title and Multi-Platform Support
    Allow for downloading the MP3 as the name of the video regardless
    of what OS the browser is being used. i.e. Windows/Unix/Linux etc...
    
    Add `delete_file()` function to clear up after completed process.
    
    Side effect: Streamlit displays `None` after completed process.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from __future__ import unicode_literals
+import json
+import os
+import time
+
 import streamlit as st
 import yt_dlp
 
@@ -18,15 +22,18 @@ class Logger(object):
         self.is_not_used()
         print(msg)
 
+
 def progress_hook_downloading(download):
     with st.spinner('Downloading'):
         if download['status'] == 'downloading':
             print('Downloading')
 
+
 def progress_hook_finished(download):
     if download['status'] == 'finished':
         st.success('All finished....')
         print('All finished....')
+
 
 youtube_dl_options = {
     'format': 'bestaudio/best',
@@ -37,8 +44,9 @@ youtube_dl_options = {
     }],
     'logger': Logger(),
     'progress_hooks': [progress_hook_downloading, progress_hook_finished],
-    'outtmpl': '~/Downloads/%(title)s-%(id)s.%(ext)s',
+    'outtmpl': '/tmp/%(title)s.%(ext)s',
 }
+
 
 if __name__ == '__main__':
 
@@ -56,26 +64,57 @@ if __name__ == '__main__':
     # Create a text input field to type or paste the video url
     input_video_url: str = st.text_input('Please enter the video url')
 
-    def downloading():
+
+    def delete_file(file_path):
+        os.path.isfile(file_path) and os.remove(file_path)
+
+
+    def wrangle_meta_data(meta_data):
+        return json.loads(json.dumps(meta_data))
+
+
+    def fetch(download_mp3: bool = True) -> str:
         """Callback function to initiate download and conversion of video to MP3
 
            Args:
-               None: None
+               download_mp3: Whether to download the MP3 while fetching the metadata
 
            Returns:
-               None: None
+               title: The title of the video extracted from the video's metadata
         """
         try:
             with st.spinner('Downloading and converting...'):
                 with yt_dlp.YoutubeDL(youtube_dl_options) as ydl:
-                    ydl.download([input_video_url])
+                    info = ydl.extract_info(input_video_url, download=download_mp3)
+                    json_data = wrangle_meta_data(ydl.sanitize_info(info))
+                return json_data['title']
         except yt_dlp.utils.DownloadError as error:
             print(error)
 
-    st.button(label='convert/download', on_click=downloading)
+
+    st.button(label='Convert', on_click=fetch)
+
+    if input_video_url:
+
+        title = fetch(download_mp3=False)
+
+        print(title)
+
+        file_path = f'/tmp/{title}.mp3'
+
+        while not os.path.exists(file_path):
+            time.sleep(5)
+            if os.path.isfile(file_path): break
+
+        with open(file_path, 'rb') as file:
+            st.download_button(
+                label='Download',
+                data=file,
+                file_name=f'{title}.mp3',
+                mime='audio/mpeg',
+            ) and delete_file(file_path)
 
 
-# Press the green button in the gutter to run the script.
 # if __name__ == '__main__':
 #     import subprocess
 #
